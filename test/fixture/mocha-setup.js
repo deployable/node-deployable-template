d97a4593446575224c79cba0659c753db6cae4b8
Matt Hoyle <matt@deployable.co>
2016-10-19 03:44:57 +0000

    templates writing to temp dir

--- test/fixture/mocha-setup.js
+++ test/fixture/mocha-setup.js
@@ -3,4 +3,4 @@ global.chai = require('chai')
 global.sinon = require('sinon')
 global.expect = chai.expect
 global.chai.use(require('chai-as-promised'))
-global.chai.config.truncateThreshold = 128
+global.chai.config.truncateThreshold = 1024
--- test/unit_template_set_spec.js
+++ test/unit_template_set_spec.js
@@ -1,9 +1,17 @@
 const TemplateSet = require('../lib/template_set')
+const Promise = require('bluebird')
 const path = require('path')
 const debug = require('debug')('dply::test::unit::module_template::template_set')
+const os = require('os')
+const fs = Promise.promisifyAll(require('fs'))
 
 
 describe('unit::module_template::template_set', function(){
+
+  let desc_fixture_dir = path.join(__dirname, 'fixture')
+  let desc_output_dir = path.join(__dirname, 'output')
+  let desc_tmpoutput_dir_prefix = path.join(desc_output_dir, 'tmp-')
+
 
   describe('TemplateSet creation', function(){
 
@@ -32,9 +40,45 @@ describe('unit::module_template::template_set', function(){
     })
 
     it('should eventually populate a set of files', function(){
-      return expect( ts.waitForFiles() ).to.eventually.have.length( 8 )
+      this.slow(20)
+      //return expect( ts.findFiles().then(f=>{console.log(f);return f}) ).to.eventually.have.length( 8 )
+      return expect( ts.findFiles() ).to.eventually.have.length( 8 )
     })
 
   })
 
+
+  describe('TemplateSet writing', function(){
+
+    let desc_temp_output_dir = null
+    let ts = null
+
+    before(function(done){
+      fs.mkdtempAsync(desc_tmpoutput_dir_prefix).then( tmpdir => {
+        ts = new TemplateSet('base', { output_path: tmpdir })
+        ts.findFiles().then(()=> done())
+      })
+    })
+
+    it('should write out templates to dir', function(){
+      return expect( ts.templateFiles() )
+        .to.eventually.have.length( 8 )
+    })
+
+  })
+
+
+  describe('TemplateSet failures', function(){
+
+    it('should error on missing templates', function(){
+      let ts = new TemplateSet('nope')
+      let template_path = path.resolve( __dirname, '..', 'template', 'nope' )
+      return expect( ts.findFiles() )
+        .to.eventually.be.rejectedWith(Error, `No such file or directory "${template_path}"`);
+    })
+
+  })
+
+
 })
+  
