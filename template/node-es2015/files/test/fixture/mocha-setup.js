7c0c02e0e7859f12572cd8de88610160a1cbcb38
Matt Hoyle <matt@deployable.co>
2016-12-08 05:13:11 +0000

    template fixes

--- template/node-es2015/files/test/fixture/mocha-setup.js
+++ template/node-es2015/files/test/fixture/mocha-setup.js
@@ -1,4 +1,11 @@
-global.mocha = require(mocha)
 global.chai = require(chai)
 global.sinon = require(sinon)
 global.expect = chai.expect
+
+require('bluebird').config({
+  longStackTraces: true,
+  warnings: true
+})
+
+if (!process.env.NODE_ENV) process.env.NODE_ENV = 'test'
+
