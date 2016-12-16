f12745845639c51f00a58a6f0db94c1f34fecf44
Matt Hoyle <matt@deployable.co>
2016-12-16 01:28:29 +0000

    move make into base dir

--- make.sh
+++ make.sh
@@ -12,20 +12,20 @@ YARN_VERSION=0.18.0
 ARGS="${@:-build}"
 
 create_versions(){
-  cp Dockerfile.base Dockerfile.6; perl -pi -e 's/alpine-node:.+/alpine-node:6/' Dockerfile.6
-  cp Dockerfile.base Dockerfile.4; perl -pi -e 's/alpine-node:.+/alpine-node:4/' Dockerfile.4
-  cp Dockerfile.base Dockerfile.7; perl -pi -e 's/alpine-node:.+/alpine-node:7/' Dockerfile.7
+  cp docker/Dockerfile.base docker/Dockerfile.6; perl -pi -e 's/alpine-node:.+/alpine-node:6/' Dockerfile.6
+  cp docker/Dockerfile.base docker/Dockerfile.4; perl -pi -e 's/alpine-node:.+/alpine-node:4/' Dockerfile.4
+  cp docker/Dockerfile.base docker/Dockerfile.7; perl -pi -e 's/alpine-node:.+/alpine-node:7/' Dockerfile.7
 }
 
 download() {
-  mkdir "$rundir"/../pkg
-  wget -nc -c -O "$rundir/../pkg/yarn-v${YARN_VERSION}.tar.gz" https://github.com/yarnpkg/yarn/releases/download/v${YARN_VERSION}/yarn-v${YARN_VERSION}.tar.gz
+  mkdir "$rundir"/pkg
+  wget -nc -c -O "$rundir/pkg/yarn-v${YARN_VERSION}.tar.gz" https://github.com/yarnpkg/yarn/releases/download/v${YARN_VERSION}/yarn-v${YARN_VERSION}.tar.gz
 }
 
 # Pack the app for docker
 pack(){
-  mkdir -p ../pkg
-  cd ../pkg
+  mkdir -p pkg
+  cd pkg
   pkg=$(npm pack ../app)
   cp $pkg deployable-template.tar.gz
   cd $rundir
@@ -37,9 +37,9 @@ build() {
   local tag=${1:-latest}
   docker build \
     --build-arg YARN_VERSION=${YARN_VERSION} \
-    --file $rundir/Dockerfile.$version \
+    --file $rundir/docker/Dockerfile.$version \
     --tag $SCOPE/$NAME:${tag} \
-    $rundir/..
+    $rundir
 }
 
 pull(){
